a12624d032b8b5ac4816a7f95678a3dfc400bbb0
Halmi Hodzhov <hhodztc@training.local>
2018-11-12 21:27:47 +0200

    modify implementation of print-all command

--- CommandInterpreter/src/commands/Print.java
+++ CommandInterpreter/src/commands/Print.java
@@ -9,42 +9,33 @@ import my_types.*;
 
 public class Print extends Command implements Executable {
 
-	
 	public Print(TypeContainer myTypeContainer) {
 		super(myTypeContainer);
 	}
 
 	@Override
 	public String execute() {
-		
+
 		StringBuilder allInformation = new StringBuilder();
 		String columns = "Variable name\t\tType\t\t\tValue\n\n";
 		allInformation.append(columns);
-		
-		
+
 		Map<String, MyType> allVariables = myTypeContainer.getVariableContainer();
-		
-		for(Map.Entry<String, MyType> currentVariable : allVariables.entrySet()) {
-			if(currentVariable.getValue() instanceof MyNumber) {
-				allInformation.append(currentVariable.getKey() + "\t\t"
-			+ currentVariable.getValue().getType() + "\t\t" + currentVariable.getValue().toString() + "\n");
+
+		for (Map.Entry<String, MyType> currentVariable : allVariables.entrySet()) {
+			String currentVariableInfo;
+			if (currentVariable.getValue() instanceof MyDate) {
+				currentVariableInfo = currentVariable.getKey() + "\t\t" + currentVariable.getValue().getType() + "\t\t\t"
+						+ currentVariable.getValue().toString() + "\n";
+				allInformation.append(currentVariableInfo);
+				allInformation.append(new String(new char[60]).replace('\0', '-')).append("\n");
+				
+			} else {
+				allInformation.append(currentVariable.getKey() + "\t\t" + currentVariable.getValue().getType() + "\t\t"
+						+ currentVariable.getValue().toString() + "\n");
+				allInformation.append(new String(new char[60]).replace('\0', '-')).append("\n");
 			}
 		}
-		for(Map.Entry<String, MyType> currentVariable : allVariables.entrySet()) {
-			if(currentVariable.getValue() instanceof MyString) {
-				allInformation.append(currentVariable.getKey() + "\t\t"
-			+ currentVariable.getValue().getType() + "\t\t" + currentVariable.getValue().toString() + "\n");
-			}
-		}
-		
-		for(Map.Entry<String, MyType> currentVariable : allVariables.entrySet()) {
-			if(currentVariable.getValue() instanceof MyDate) {
-				allInformation.append(currentVariable.getKey() + "\t\t"
-			+ currentVariable.getValue().getType() + "\t\t\t" + currentVariable.getValue().toString() + "\n");
-			}
-		}
-		
 		return allInformation.toString();
 	}
-
 }
